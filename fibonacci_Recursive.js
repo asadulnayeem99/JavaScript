421cf6776a1e3e8702469378f80e714d7cd68af7
Md Asadul Islam Nayeem <asadulnayeem99@gmail.comm>
2023-08-13 15:17:41 +0600

    New

---
+++ fibonacci_Recursive.js
@@ -0,0 +1,13 @@
+function fibonacci(n) {
+  if (n == 1) {
+    return 1;
+  }
+  if(n == 0){
+    return 0;
+  }
+  else {
+    return fibonacci(n-1) + fibonacci(n-2);
+  }
+}
+let num = fibonacci(10);
+console.log(num);
