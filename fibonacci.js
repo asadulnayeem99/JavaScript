a4c930e61002a1167123957d82ab7fd3900baa0a
Md Asadul Islam Nayeem <asadulnayeem99@gmail.comm>
2023-08-13 15:01:29 +0600

    JS

---
+++ fibonacci.js
@@ -0,0 +1,10 @@
+function fibonacci(n) {
+  var fibo = [0, 1];
+  for (let i = 2; i <= n; i++) {
+    fibo[i] = fibo[i - 1] + fibo[i - 2];
+    console.log(fibo[i],fibo[i-1],fibo[i-2]);
+  }
+  return fibo;
+}
+let num = fibonacci(15);
+console.log(num);
